24a5f184bb46a21645d9464d4c8c8e406fa66587
supa-modo <eddieochieng11@gmail.com>
2025-03-09 10:58:31 +0300

    Add Vite and Vercel config for client-side routing

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,25 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    port: 3000,
+    open: true,
+  },
+  build: {
+    outDir: "dist",
+    rollupOptions: {
+      output: {
+        manualChunks: (id) => {
+          if (id.includes("node_modules")) {
+            return "vendor";
+          }
+        },
+      },
+    },
+  },
+  base: "/",
+  preview: {
+    port: 3000,
+    strictPort: true,
+  },
 });
